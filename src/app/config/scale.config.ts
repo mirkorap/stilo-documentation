0c4b892a1ba1df815ba924cf65e9aeb929487b70
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-22 10:58:14 +0200

    feat: add documentation for spacing scale

---
+++ src/app/config/scale.config.ts
@@ -0,0 +1,9 @@
+import { InjectionToken } from '@angular/core';
+
+export const SCALE_CONFIG = new InjectionToken<number[]>('scale.config');
+
+export const SPACING_SCALE_UNIT = 4;
+
+export const SPACING_SCALE_CONFIG = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72, 80, 96
+];
--- src/app/pages/spacing-scale-page/spacing-scale-page.component.ts
+++ src/app/pages/spacing-scale-page/spacing-scale-page.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { SCALE_CONFIG, SPACING_SCALE_CONFIG, SPACING_SCALE_UNIT } from '@app/config/scale.config';
 
 @Component({
   selector: 'app-spacing-scale-page',
   templateUrl: './spacing-scale-page.component.html',
   styleUrls: ['./spacing-scale-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: SCALE_CONFIG, useValue: SPACING_SCALE_CONFIG }]
 })
 export class SpacingScalePageComponent implements OnInit {
+  config: number[] = [];
+  columns: string[] = ['name', 'size'];
+  rows: { [key: string]: any }[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Inject(SCALE_CONFIG) config: number[]) {
+    this.config = config;
   }
 
+  ngOnInit(): void {
+    this.rows = this.config.map((value) => {
+      const size = value * SPACING_SCALE_UNIT;
+      return { name: `space${value}`, size: `${size}px` };
+    });
+  }
 }
