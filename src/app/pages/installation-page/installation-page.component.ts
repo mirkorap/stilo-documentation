6785b66254d38e86842e4a732770f6e6466acffb
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-22 08:46:56 +0200

    feat: add documentation for installation

--- src/app/pages/installation-page/installation-page.component.ts
+++ src/app/pages/installation-page/installation-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-installation-page',
@@ -6,11 +6,4 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./installation-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InstallationPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class InstallationPageComponent {}
