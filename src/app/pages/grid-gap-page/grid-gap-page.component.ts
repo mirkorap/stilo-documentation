1e25cfd443c36ab7d0029be7f48e4e88557a4499
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-23 16:46:49 +0200

    feat: add documentation for StiloGridGap

--- src/app/pages/grid-gap-page/grid-gap-page.component.ts
+++ src/app/pages/grid-gap-page/grid-gap-page.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { SCALE_CONFIG, SPACING_SCALE_CONFIG, SPACING_SCALE_UNIT } from '@app/config/scale.config';
 
 @Component({
   selector: 'app-grid-gap-page',
   templateUrl: './grid-gap-page.component.html',
   styleUrls: ['./grid-gap-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: SCALE_CONFIG, useValue: SPACING_SCALE_CONFIG }]
 })
 export class GridGapPageComponent implements OnInit {
+  config: number[] = [];
+  columns: string[] = ['name', 'gap'];
+  rows: { [key: string]: any }[] = [];
+  usageExample = `
+  GridView.count(
+    crossAxisCount: 5,
+    mainAxisSpacing: StiloGridGap.gap10,
+    crossAxisSpacing: StiloGridGap.gap2,
+    children: List.generate(100, (index) {
+      return Center(
+        child: Text('Item $index'),
+      );
+    }),
+  )`;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Inject(SCALE_CONFIG) config: number[]) {
+    this.config = config;
   }
 
+  ngOnInit(): void {
+    this.rows = this.config.map((value) => {
+      const size = value * SPACING_SCALE_UNIT;
+      return { name: `gap${value}`, gap: `${size}px` };
+    });
+  }
 }
