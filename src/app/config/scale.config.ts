4e1ee5b3aa84e03d1b017662aebc913f1263fbdd
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-23 14:32:57 +0200

    feat: add documentation for StiloElevation

--- src/app/config/scale.config.ts
+++ src/app/config/scale.config.ts
@@ -7,3 +7,5 @@ export const SPACING_SCALE_UNIT = 4;
 export const SPACING_SCALE_CONFIG = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72, 80, 96
 ];
+
+export const ELEVATION_SCALE_CONFIG = [0, 10, 20, 30, 40, 50];
--- src/app/pages/elevation-page/elevation-page.component.ts
+++ src/app/pages/elevation-page/elevation-page.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { ELEVATION_SCALE_CONFIG, SCALE_CONFIG } from '@app/config/scale.config';
 
 @Component({
   selector: 'app-elevation-page',
   templateUrl: './elevation-page.component.html',
   styleUrls: ['./elevation-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: SCALE_CONFIG, useValue: ELEVATION_SCALE_CONFIG }]
 })
 export class ElevationPageComponent implements OnInit {
+  config: number[] = [];
+  columns: string[] = ['name', 'elevation'];
+  rows: { [key: string]: any }[] = [];
+  usageExample = `
+  Card(
+    elevation: StiloElevation.z10,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const ListTile(
+          title: Text('Hello World'),
+        ),
+      ],
+    ),
+  )`;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Inject(SCALE_CONFIG) config: number[]) {
+    this.config = config;
   }
 
+  ngOnInit(): void {
+    this.rows = this.config.map((value) => {
+      return { name: `z${value}`, elevation: value };
+    });
+  }
 }
