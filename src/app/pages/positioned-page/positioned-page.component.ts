9344b679d3ac65fc25f6f51b91038f462d03a267
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-23 16:30:59 +0200

    feat: add documentation for StiloPositioned

--- src/app/pages/positioned-page/positioned-page.component.ts
+++ src/app/pages/positioned-page/positioned-page.component.ts
@@ -1,16 +1,77 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { SCALE_CONFIG, SPACING_SCALE_CONFIG, SPACING_SCALE_UNIT } from '@app/config/scale.config';
 
 @Component({
   selector: 'app-positioned-page',
   templateUrl: './positioned-page.component.html',
   styleUrls: ['./positioned-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: SCALE_CONFIG, useValue: SPACING_SCALE_CONFIG }]
 })
 export class PositionedPageComponent implements OnInit {
+  config: number[] = [];
+  columns: string[] = ['name', 'position'];
+  rows: { [key: string]: any }[] = [];
+  positionTypes: { name: string; position: string }[] = [
+    {
+      name: 'inset',
+      position: 'top: X; right: X; bottom: X; left: X'
+    },
+    {
+      name: 'insetY',
+      position: 'top: X; bottom: X'
+    },
+    {
+      name: 'insetX',
+      position: 'right: X; left: X'
+    },
+    {
+      name: 'top',
+      position: 'top: X'
+    },
+    {
+      name: 'right',
+      position: 'right: X'
+    },
+    {
+      name: 'bottom',
+      position: 'bottom: X'
+    },
+    {
+      name: 'left',
+      position: 'left: X'
+    }
+  ];
+  usageExample = `
+  Stack(
+    children: [
+      Container(
+        width: StiloWidth.w64,
+        height: StiloHeight.h64,
+        color: StiloColor.blue[500],
+      ),
+      StiloPositioned.inset12(
+        child: Container(
+          width: StiloWidth.w32,
+          height: StiloHeight.h32,
+          color: StiloColor.red[500],
+        ),
+      ),
+    ],
+  )`;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Inject(SCALE_CONFIG) config: number[]) {
+    this.config = config;
   }
 
+  ngOnInit(): void {
+    this.rows = this.config.flatMap((value) => {
+      return this.positionTypes.map((positionType) => {
+        const size = value * SPACING_SCALE_UNIT;
+        const name = positionType.name + size;
+        const position = positionType.position.replace(/X/g, size.toString());
+        return { name, position };
+      });
+    });
+  }
 }
