92276e18045a192a92c51a79dbbbbc4bfc5bd228
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-29 14:31:40 +0200

    feat: add documentation for StiloColorOpacity

--- src/app/config/scale.config.ts
+++ src/app/config/scale.config.ts
@@ -9,3 +9,5 @@ export const SPACING_SCALE_CONFIG = [
 ];
 
 export const ELEVATION_SCALE_CONFIG = [0, 10, 20, 30, 40, 50];
+
+export const COLOR_OPACITY_SCALE_CONFIG = [0, 5, 10, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 95, 100];
--- src/app/pages/color-opacity-page/color-opacity-page.component.ts
+++ src/app/pages/color-opacity-page/color-opacity-page.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { COLOR_OPACITY_SCALE_CONFIG, SCALE_CONFIG } from '@app/config/scale.config';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-color-opacity-page',
   templateUrl: './color-opacity-page.component.html',
   styleUrls: ['./color-opacity-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: SCALE_CONFIG, useValue: COLOR_OPACITY_SCALE_CONFIG }]
 })
 export class ColorOpacityPageComponent implements OnInit {
+  config: number[] = [];
+  columns: string[] = ['name', 'opacity'];
+  rows: { [key: string]: any }[] = [];
+  usageExample = `
+  Center(
+    child: Row(
+      children: [
+        Container(
+          width: StiloWidth.w64,
+          height: StiloHeight.h36,
+          color: StiloColor.blue[500].withOpacity(StiloColorOpacity.opacity50),
+        ),
+        Container(
+          width: StiloWidth.w36,
+          height: StiloHeight.h64,
+          color: StiloColor.red[500].withOpacity(StiloColorOpacity.opacity95),
+        ),
+      ],
+    ),
+  )`;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Inject(SCALE_CONFIG) config: number[]) {
+    this.config = config;
   }
 
+  ngOnInit(): void {
+    this.rows = this.config.map((value) => {
+      return { name: `opacity${value}`, opacity: value };
+    });
+  }
 }
