b254e1164cf2f093066f293de088acc0712dd023
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-29 10:17:32 +0200

    feat: add documentation for StiloFontSize

--- src/app/pages/font-size-page/font-size-page.component.ts
+++ src/app/pages/font-size-page/font-size-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-font-size-page',
@@ -6,11 +6,58 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./font-size-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FontSizePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FontSizePageComponent {
+  config: number[] = [];
+  columns: string[] = ['name', 'fontsize'];
+  rows: { [key: string]: any }[] = [
+    {
+      name: 'xs',
+      fontsize: '9px'
+    },
+    {
+      name: 'sm',
+      fontsize: '10.5px'
+    },
+    {
+      name: 'md',
+      fontsize: '12px'
+    },
+    {
+      name: 'lg',
+      fontsize: '13.5px'
+    },
+    {
+      name: 'xl',
+      fontsize: '15px'
+    },
+    {
+      name: 'xl2',
+      fontsize: '18px'
+    },
+    {
+      name: 'xl3',
+      fontsize: '22.5px'
+    },
+    {
+      name: 'xl4',
+      fontsize: '27px'
+    },
+    {
+      name: 'xl5',
+      fontsize: '36px'
+    },
+    {
+      name: 'xl6',
+      fontsize: '48px'
+    }
+  ];
+  usageExample = `
+  Center(
+    child: Text(
+      'Hello World',
+      style: TextStyle(
+        fontSize: StiloFontSize.xl4,
+      ),
+    ),
+  )`;
 }
