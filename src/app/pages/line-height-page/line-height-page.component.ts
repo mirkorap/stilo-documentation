ef95a26b6928c633e0ef5841177cb3ec5dedc577
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-29 10:33:37 +0200

    feat: add documentation for StiloLineHeight

--- src/app/pages/line-height-page/line-height-page.component.ts
+++ src/app/pages/line-height-page/line-height-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-line-height-page',
@@ -6,11 +6,47 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./line-height-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LineHeightPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class LineHeightPageComponent {
+  config: number[] = [];
+  columns: string[] = ['name', 'height'];
+  rows: { [key: string]: any }[] = [
+    {
+      name: 'none',
+      height: '1.0'
+    },
+    {
+      name: 'tight',
+      height: '1.25'
+    },
+    {
+      name: 'snug',
+      height: '1.375'
+    },
+    {
+      name: 'normal',
+      height: '1.5'
+    },
+    {
+      name: 'relaxed',
+      height: '1.625'
+    },
+    {
+      name: 'loose',
+      height: '2.0'
+    }
+  ];
+  usageExample = `
+  Center(
+    child: Text(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      'Curabitur lectus enim, porttitor ac ultrices quis, scelerisque sed massa.'
+      'Phasellus ultrices tortor in ipsum sodales, id feugiat arcu maximus.',
+      style: TextStyle(
+        fontSize: StiloFontSize.xl4,
+        fontWeight: StiloFontWeight.extraBold,
+        letterSpacing: StiloLetterSpacing.widest,
+        height: StiloLineHeight.relaxed,
+      ),
+    ),
+  )`;
 }
