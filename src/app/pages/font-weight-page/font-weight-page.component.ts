92a2a4ea92733190462cca4930b95da29b90c297
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-29 10:23:41 +0200

    feat: add documentation for StiloFontWeight

--- src/app/pages/font-weight-page/font-weight-page.component.ts
+++ src/app/pages/font-weight-page/font-weight-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-font-weight-page',
@@ -6,11 +6,55 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./font-weight-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FontWeightPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FontWeightPageComponent {
+  config: number[] = [];
+  columns: string[] = ['name', 'weight'];
+  rows: { [key: string]: any }[] = [
+    {
+      name: 'thin',
+      weight: '100'
+    },
+    {
+      name: 'extraLight',
+      weight: '200'
+    },
+    {
+      name: 'light',
+      weight: '300'
+    },
+    {
+      name: 'regular',
+      weight: '400'
+    },
+    {
+      name: 'medium',
+      weight: '500'
+    },
+    {
+      name: 'semiBold',
+      weight: '600'
+    },
+    {
+      name: 'bold',
+      weight: '700'
+    },
+    {
+      name: 'extraBold',
+      weight: '800'
+    },
+    {
+      name: 'black',
+      weight: '900'
+    }
+  ];
+  usageExample = `
+  Center(
+    child: Text(
+      'Hello World',
+      style: TextStyle(
+        fontSize: StiloFontSize.xl4,
+        fontWeight: StiloFontWeight.extraBold,
+      ),
+    ),
+  )`;
 }
